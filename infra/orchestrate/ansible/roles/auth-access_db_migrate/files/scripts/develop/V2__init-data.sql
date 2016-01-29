e7567925eedd631dc4189c728081531106176c94
mts_n <nantsuka2011@gmail.com>
2016-01-29 22:29:54 +0900

    不要になった古いオーケストレーションを削除

--- infra/orchestrate/ansible/roles/auth-access_db_migrate/files/scripts/develop/V2__init-data.sql
+++
@@ -1,1 +0,0 @@
-insert into event_processes (id, type, last_event_id) values (1, 'delegate', 0);
--- infra/orchestrate/ansible/roles/widget-store_db_migrate/files/scripts/develop/V1.2__schema-id.sql
+++
@@ -1,3 +0,0 @@
-alter table events add column id bigserial primary key;
-alter table event_processes add column id bigserial primary key;
-alter table widgets add column id serial primary key;
